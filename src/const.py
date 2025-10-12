8119e6793500701b2ddae6903bb76b8ae81b9960
Julian Bednarek <juliuszxxbednarek@gmail.com>
2025-10-12 13:55:13 +0200

    new gitingore sketch of model

--- src/const.py
+++ src/const.py
@@ -3,15 +3,18 @@
 from enum import Enum
 import torch
 
+
 class ImageTypes(Enum):
     """Types of images in our dataset."""
+
     NORMAL = ""
     BW = "-bw"
     GRAY = "-gray"
 
+
 IMAGE_SIZE = 64
 IMAGE_TYPE = ImageTypes.NORMAL.value
 IMAGE_EXTENSION = ".jpg"
-TRAIN_DATA_PATH = b"../datasets/Hey-Waldo/%d%s/notwaldo" % (IMAGE_SIZE, IMAGE_TYPE)
-TEST_DATA_PATH = b"../datasets/Hey-Waldo/%d%s/waldo" % (IMAGE_SIZE, IMAGE_TYPE)
+TRAIN_DATA_PATH = f"../datasets/Hey-Waldo/{IMAGE_SIZE}{IMAGE_TYPE}/notwaldo"
+TEST_DATA_PATH = f"../datasets/Hey-Waldo/{IMAGE_SIZE}{IMAGE_TYPE}/waldo"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
